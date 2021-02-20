da11ee9cf57e42d9b7564b4d521cfbded2bebaee
Marcos Correia <mc021970@bol.com.br>
2021-02-19 21:24:03 -0300

    REST Categorias

--- src/main/java/br/com/mc/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/mc/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package br.com.mc.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.mc.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	public static List<Categoria> categorias = new ArrayList<>();
+	
+	static {
+
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		categorias.add(c1);
+		categorias.add(c2);
+	}
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest esta funcionando";
+	public List<Categoria> listar() {
+		return categorias;
 	}
 	
 }
