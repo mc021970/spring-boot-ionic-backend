2d25175afbd06228e68cc3497b41aa78baf07284
Marcos Correia <mc021970@bol.com.br>
2021-03-04 22:50:02 -0300

    Erro de validacao

---
+++ src/main/java/br/com/mc/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,25 @@
+package br.com.mc.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		FieldMessage error = new FieldMessage(fieldName, message);
+		this.errors.add(error);
+	}
+
+	
+}
