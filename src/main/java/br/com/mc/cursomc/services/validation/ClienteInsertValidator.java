f6dfc3d89b97fd6a7167c1e362532175875f0ffe
Marcos Correia <mc021970@bol.com.br>
2021-03-10 00:12:51 -0300

    Validacao CPF/CNPJ

---
+++ src/main/java/br/com/mc/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,58 @@
+package br.com.mc.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.hibernate.validator.internal.constraintvalidators.hv.br.CNPJValidator;
+import org.hibernate.validator.internal.constraintvalidators.hv.br.CPFValidator;
+
+import br.com.mc.cursomc.domain.enums.TipoCliente;
+import br.com.mc.cursomc.dto.ClienteNewDTO;
+import br.com.mc.cursomc.resources.exception.FieldMessage;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> errors = new ArrayList<>();
+		
+		// inclua os testes aqui, inserindo erros na lista
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo())) {
+			System.out.println("Pessoa Física");
+			CPFValidator cpfv = new CPFValidator();
+			cpfv.initialize(null);
+			boolean cpfValido = cpfv.isValid(objDto.getCpfOuCnpj(), context);
+			System.out.println("CPF válido: " + cpfValido);
+			if (!cpfValido) {
+				errors.add(new FieldMessage("cpfOuCnpj", "Campo CPF inválido"));
+			}
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo())) {
+			System.out.println("Pessoa Juridica");
+			
+			CNPJValidator cnpjv = new CNPJValidator();
+			cnpjv.initialize(null);
+			boolean cnpjValido = cnpjv.isValid(objDto.getCpfOuCnpj(), context);
+			
+			System.out.println("CNPJ válido: " + cnpjValido);
+			if (!cnpjValido) {
+				errors.add(new FieldMessage("cpfOuCnpj", "Campo CNPJ inválido"));
+			}
+		}
+
+		for (FieldMessage e : errors) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return errors.isEmpty();
+	}
+}
